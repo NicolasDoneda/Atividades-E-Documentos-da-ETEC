958eb4df33a221d1cfb836d11191a073ff28569d
NicolasDoneda <nicolas.nascimento43@etec.sp.gov.br>
2025-04-11 19:53:49 -0300

    Update Atividade aula 09 - 04 (métodos em C#).cs

--- Desenvolvimento de Sistemas I/Exercícios/Atividade aula 09 - 04 (métodos em C#).cs
+++ Desenvolvimento de Sistemas I/Exercícios/Atividade aula 09 - 04 (métodos em C#).cs
@@ -1,46 +1,13 @@
 using System;
 
-
 class Program
 {
-
-
-
     static void Main()
     {
-
-
-//-------------------------------------------------------------------------//
-        static int SomarTresNumeros(int num1, int num2, int num3)
-        {
-            return num1 + num2 + num3;
-
-        }
-//-------------------------------------------------------------------------//
-        void Saudacao (string nome)
-        {
-            Console.WriteLine("Olá, "+ nome+ "! Bem-Vindo.");
-        }
-//-------------------------------------------------------------------------//
-
-        static string EhMultiploDeCinco(int numero)
-        {
-            if (numero % 5 == 0)
-            {
-                return "Sim";
-            }
-            else
-            {
-                return "Não";
-            }
-        }
-//-------------------------------------------------------------------------//
-
         Console.WriteLine("Digite os três números para serem somados: ");
         int n1 = Convert.ToInt32(Console.ReadLine());
         int n2 = Convert.ToInt32(Console.ReadLine());
         int n3 = Convert.ToInt32(Console.ReadLine());
-
 
         int resultado = SomarTresNumeros(n1, n2, n3);
         Console.WriteLine("O resultado é: \n" + resultado);
@@ -51,5 +18,27 @@ class Program
         int numeroParaVerificar = Convert.ToInt32(Console.ReadLine());
         string resposta = EhMultiploDeCinco(numeroParaVerificar);
         Console.WriteLine("O número " + numeroParaVerificar + " é múltiplo de 5? \n" + resposta);
+    }
+
+    static int SomarTresNumeros(int num1, int num2, int num3)
+    {
+        return num1 + num2 + num3;
+    }
+
+    static void Saudacao(string nome)
+    {
+        Console.WriteLine("Olá, " + nome + "! Bem-Vindo.");
+    }
+
+    static string EhMultiploDeCinco(int numero)
+    {
+        if (numero % 5 == 0)
+        {
+            return "Sim";
+        }
+        else
+        {
+            return "Não";
+        }
     }
 }
